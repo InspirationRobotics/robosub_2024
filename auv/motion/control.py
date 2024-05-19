5bac9876022d172a4f40fea09edf4543089c9208
Dat-Bois <eeshvij@gmail.com>
2024-05-19 15:52:47 -0700

    Changes to the code

--- auv/motion/control.py
+++ auv/motion/control.py
@@ -25,27 +25,27 @@ rc = robot_control.RobotControl()
 def control():
     global forward, lateral, yaw, drop, run
     var = input("Type in a command: ")
-    if var == 'forward':
-        forward = 2
-    elif var == 'backward':
-        forward = -2
-    elif var == 'down':
-        drop = 2
-    elif var == 'up':
-        drop = -2
-    elif var == 'lateral right':
-        lateral = 2 # Right
-    elif var == 'lateral left':
-        lateral = -2
-    elif var == 'yaw cw':
-        yaw = 2 # Clockwise
-    elif var == 'yaw ccw':
-        yaw = -2
-    elif var == 'stop forward':
+    if var == 'f':
+        forward = 1
+    elif var == 'b':
+        forward = -1
+    elif var == 'd':
+        drop = 1
+    elif var == 'u':
+        drop = -1
+    elif var == 'lr':
+        lateral = 1 # Right
+    elif var == 'll':
+        lateral = -1
+    elif var == 'y cw':
+        yaw = 0.5 # Clockwise
+    elif var == 'y ccw':
+        yaw = -0.5
+    elif var == 'sf':
         forward = 0
-    elif var == 'stop lateral':
+    elif var == 'sl':
         lateral = 0
-    elif var == 'stop yaw':
+    elif var == 'y':
         yaw = 0
     elif var == "stop drop":
         drop = 0
@@ -61,12 +61,7 @@ def control():
         while True:
             stop_input = input("Key in 'stop' to keep the depth of the sub: ")
             if stop_input == "stop":
-                break
-            elif time.time() - start_time == 0.5:
-                start_time = time.time()
-                drop += 0.2
-                print(f'{drop}')
-                
+                break               
         # depth = input("Absolute depth: ")
         # rc.set_depth(depth)
     elif var == 'i':
