7f86a4de4c727945013ce7c720c3899501c2dc03
Brandon Tran <141108900+bug3x@users.noreply.github.com>
2024-06-11 15:27:32 -0700

    Update bin_test_cv.py

--- auv/cv/test/bin_test_cv.py
+++ auv/cv/test/bin_test_cv.py
@@ -11,7 +11,7 @@ class CV:
     """
   
     camera = "/auv/camera/videoOAKdRawBottom"
-    model = "bins3"
+    model = "bin"
     
     def __init__(self, **config):
         """Initialize CV class"""
@@ -20,50 +20,6 @@ class CV:
         self.error_buffer = []
       
         print("[INFO] Bin CV Init")
-
-    def mask_out_lids(self, target, lids):
-        """
-        target: the target bin
-        lids: the list of lids
-        returns a new target bin with the lids masked out
-        """
-
-        if len(lids) == 0:
-            return target, self.get_bbox_center(target)
-
-        x1, x2, y1, y2 = target.xmin, target.xmax, target.ymin, target.ymax
-        geom_target = shapely.geometry.box(x1, y1, x2, y2)
-
-        for lid in lids:
-            x1, x2, y1, y2 = lid.xmin - 100, lid.xmax + 100, lid.ymin, lid.ymax
-            geom_lid = shapely.geometry.box(x1, y1, x2, y2)
-            geom_target = geom_target.difference(geom_lid)
-
-        # if there are multiple polygons, pick the largest one
-        if geom_target.geom_type == "MultiPolygon":
-            geom_target = max(geom_target, key=lambda x: x.area)
-
-        # if there is a hole in the Polygon, split it into two
-        if geom_target.geom_type == "Polygon" and geom_target.interiors:
-            x1, x2, y1, y2 = geom_target.interiors[0].bounds
-            split_line = shapely.geometry.LineString([(0, (y1 + y2) / 2), (640, (y1 + y2) / 2)])
-
-            # split the polygon with the line
-            splitted = shapely.ops.split(geom_target, split_line)
-
-            # pick the largest polygon
-            geom_target = max(splitted, key=lambda x: x.area)
-
-        x1, y1, x2, y2 = geom_target.bounds
-        target.xmin = x1
-        target.ymin = y1
-        target.xmax = x2
-        target.ymax = y2
-
-        # get the centroid
-        centroid = geom_target.centroid
-        target_center = (int(centroid.x), int(centroid.y))
-        return target, target_center
 
     def get_bbox_center(self, detection):
         x1 = int(detection.xmin)
@@ -97,13 +53,13 @@ class CV:
 
         tolerance = 0.1
         maxConfidence = 0
-        target_bin = None
+        target_color = None
         red = None
         blue = None
         red_confidence = 0
         blue_confidence = 0
-        bins = []
-        lids = []
+        bin = []
+        colors = []
 
         # measured offset from the footage
         target_pixel = (190, 300)
@@ -125,50 +81,40 @@ class CV:
                 red = detection
                 red_confidence = detection.confidence
 
-            elif "bin" in detection.label:
-                bins.append(detection)
-
-            elif "lid" in detection.label:
-                lids.append(detection)
-
         if "red" in target:
-            target_bin = red
+            target_color = red
 
         elif "blue" in target:
-            target_bin = blue
+            target_color = blue
 
         approach = False
-        if target_bin is None:
+        if target_color is None:
             if red is None and blue is None:
-                if len(bins) == 0:
+                if len(bin) == 0:
                     return {"forward": 0.8}, frame
                 else:
-                    # average the positions of the bins
-                    avg_center = np.mean([self.get_bbox_center(b) for b in bins], axis=0)
-                    target_bin_center = (int(avg_center[0]), int(avg_center[1]))
+                    # average the positions of the bin
+                    avg_center = np.mean([self.get_bbox_center(b) for b in bin], axis=0)
+                    target_color_center = (int(avg_center[0]), int(avg_center[1]))
                     approach = True
             elif red is None:
-                target_bin = blue
+                target_color = blue
             elif blue is None:
-                target_bin = red
+                target_color = red
 
         if not approach:
-            # remove the lids from the target bin
-            target_bin, target_bin_center = self.mask_out_lids(target_bin, lids)
+            pass
 
         cv2.rectangle(
             frame,
-            (int(target_bin.xmin), int(target_bin.ymin)),
-            (int(target_bin.xmax), int(target_bin.ymax)),
+            (int(target_color.xmin), int(target_color.ymin)),
+            (int(target_color.xmax), int(target_color.ymax)),
             (255, 0, 0),
             2,
         )
 
-        cv2.circle(frame, target_pixel, 10, (0, 255, 0), -1)
-        cv2.circle(frame, target_bin_center, 10, (0, 0, 255), -1)
-
-        x_error = (target_bin_center[0] - target_pixel[0]) / width
-        y_error = (target_pixel[1] - target_bin_center[1]) / height
+        x_error = (target_color_center[0] - target_pixel[0]) / width
+        y_error = (target_pixel[1] - target_color_center[1]) / height
 
         # apply a gain and clip the values
         lateral = np.clip(x_error * 3.5, -1, 1)
@@ -182,7 +128,7 @@ class CV:
         if avg_error < tolerance and len(self.error_buffer) == 30:
             aligned = True
 
-        # TODO (low priority): Remove Lids for each bin
+        # TODO (low priority): Remove colors for each bin
         return {"lateral": lateral, "forward": forward, "aligned": aligned}, frame
 
 if __name__ == "__main__":
