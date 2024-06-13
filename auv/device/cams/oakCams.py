a663fb2e0eda02b52dfd306ff17b65dc55e26a75
SidKam11 <88258270+SidKam11@users.noreply.github.com>
2024-06-12 17:10:40 -0700

    Update oakCams.py

--- auv/device/cams/oakCams.py
+++ auv/device/cams/oakCams.py
@@ -267,8 +267,7 @@ class oakCamera:
         Returns:
             modelPath (str): the path to the model
         """
-        # root = os.path.normpath("/home/inspiration/auv/auv/device/cams/models/") -- For 2023 directory
-        root = os.path.normpath("/home/inspiration/2024/robosub_2024/auv/device/cams/models/") # 2024 directory to access models
+        root = os.path.normpath("/home/inspiration/auv/auv/device/cams/models/")
 
         if modelName == "raw":
             modelPath = "raw"
