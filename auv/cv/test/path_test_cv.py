eac831e54a83713b4ede78e4bffba9df7b2427a3
Avika Prasad <89167794+avikaprasad22@users.noreply.github.com>
2024-06-13 16:43:11 -0700

    Update path_test_cv.py

--- auv/cv/test/path_test_cv.py
+++ auv/cv/test/path_test_cv.py
@@ -33,23 +33,29 @@ class CV:
 
     def detect_path(self, frame):
         detected = False
+        
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
         lower_orange_mask = np.array([10, 100, 100])
         upper_orange_mask = np.array([25, 255, 255])
 
-        mask = cv2.inRange(hsv, lower_orange_mask, upper_orange_mask)
+        mask = cv2.inRange(frame, lower_orange_mask, upper_orange_mask)
 
         contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(hsv, contours, -1, (0, 255, 0), 3)
 
-        if contours:
-            largest_contour = max(contours, key = cv2.contourArea)
+        # cv2.imshow('hsv', image)
+        # cv2.waitKey(0)
+        # cv2.destoryAllWindows()
 
-            if cv2.contourArea(largest_contour) > 0:
-                x, y, w, h = cv2.boundingRect(largest_contour)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                detected = True
-                return {"status": detected, "xmin" : x, "xmax" : (x + w), "ymin" : (y), "ymax" : (y + h)}, frame
+       # if contours:
+           # largest_contour = max(contours, key = cv2.contourArea)
+
+            #if cv2.contourArea(largest_contour) > 0:
+                #x, y, w, h = cv2.boundingRect(largest_contour)
+               # cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                #detected = True
+                #return {"status": detected, "xmin" : x, "xmax" : (x + w), "ymin" : (y), "ymax" : (y + h)}, hsv
         
         return {"status": detected, "xmin" : None, "xmax" : None, "ymin" : None, "ymax" : None}, frame 
     
@@ -59,21 +65,67 @@ class CV:
         # Yaw so that path is |, then lateral to midpoint, then yaw again.
         pass
 
-    def run(self, raw_frame):
+    def run(self, frame):
         """ Run the CV logic. Returns the motion commands and visualized frame. """
+        # converts the frame from color to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+         # converts the color values for BGR to HSV
+        into_hsv =(cv2.cvtColor(frame,cv2.COLOR_BGR2HSV))
+
+         # lowest limit to say it is orange
+        lower_limit=np.array([25, 41, 98])
+         # 3, 20, 20 
+
+         # highest limit to say it is orange
+        upper_limit=np.array([23, 100, 82])
+         # 80, 255, 255
+
+        kernel = np.ones((5, 5), np.uint8)
+
+        # takes the frame and turns the pixels in the limit to white (255), outside of limit = black (0)
+        orange=cv2.inRange(into_hsv,lower_limit,upper_limit)
+
+        # determines the edges by determining which are stong and weak
+        edges = cv2.Canny(gray, threshold1=100, threshold2=200)
+
+        ret, thresh3 = cv2.threshold(edges, 230, 255, cv2.THRESH_BINARY)
+        ret2, thresh3 = cv2.threshold(thresh3, 1, 255, cv2.THRESH_OTSU)
+        cv2.imshow("edges", thresh3)
+         # Removing Noise
+        orange = cv2.morphologyEx(orange, cv2.MORPH_OPEN, kernel)
+        time.sleep(0.2)
+         # Blur and Threshold 
+        orange = cv2.GaussianBlur(orange, (11, 11), 0)
+        ret, thresh = cv2.threshold(orange, 230, 255, cv2.THRESH_BINARY)
+
+        blur = cv2.blur(thresh, (10, 10))
+        ret2, thresh2 = cv2.threshold(blur, 1, 255, cv2.THRESH_OTSU)
+
+         # Find Contours
+        contours, heirarchy = cv2.findContours(thresh3, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(frame, contours, -1, 255, 3)
+
+        yaw = lateral = forward = 0
+        hline = cv2.line(frame, (640, 0), (0, 0), (0, 255, 255), 8)
 
         lateral = 0
         forward = 0
         yaw = 0
 
-        path_data, frame = self.detect_path(raw_frame)
+        center_line = (hline)/2
 
-        if path_data.get('status') == True:
-            self.detected = True
-            detection_xmin = path_data.get('xmin')
-            detection_xmax = path_data.get('xmax')
-            detection_ymin = path_data.get('ymin')
-            detection_ymax = path_data.get('ymax')
+        if center_line < 5:
+            lateral = 3
+
+        # path_data, frame = self.detect_path(raw_frame)
+
+        # if path_data.get('status') == True:
+        #     self.detected = True
+        #     detection_xmin = path_data.get('xmin')
+        #     detection_xmax = path_data.get('xmax')
+        #     detection_ymin = path_data.get('ymin')
+        #     detection_ymax = path_data.get('ymax')
 
         if self.step == 0 and self.detected == True:
             pass
@@ -94,9 +146,9 @@ if __name__ == "__main__":
     # another team member needs to run this code on his/her device. 
     
     # NOTE: When downloading the training data, the training data folder itself, which contains all of the data.
-    video_root_path = "/home/kc/Desktop/Team Inspiration/RoboSub 2024/Training Data/" # Computer path through the training data folder.
+    video_root_path = "/Users/avikaprasad/Desktop/RoboSub 2024/Training Data" # Computer path through the training data folder.
     mission_name = "Follow the Path/" # Mission folder
-    video_name = "Follow the Path Video 1.mp4" # Specified video
+    video_name = "Follow the Path Video 4.mp4" # Specified video
     video_path = os.path.join(video_root_path, mission_name, video_name)
 
     # For testing
