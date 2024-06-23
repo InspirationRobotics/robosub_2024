015bfa414ba5e5ec3b5ed0a6e2f23f9ee247f051
CS50HarvardKC <networkmail.kc@gmail.com>
2024-06-23 12:00:45 -0700

    Started writing mission executable CV code for the Gate mission

--- auv/cv/gate_cv.py
+++ auv/cv/gate_cv.py
@@ -33,9 +33,35 @@ class CV:
             config: Dictionary that contains the configuration of the devices on the sub.
         """
 
-        self.shape = (480, 640)
+        self.config = config
+        self.shape = (640, 480)
+        self.end = False
+
+        self.state = None
+        self.detected = False
+        self.tolerance = 20 # Pixels
 
         print("[INFO] Gate CV init")
+
+    def yaw_smart(self, detection):
+        """Determine yaw either clockwise or counterclockwise if there is one detection on the screen, based on where the detection is on the screen."""
+
+        detection_midpoint = (detection.xmin + detection.xmax)/2
+        midpoint_frame = self.shape[0]/2
+
+        # If detection is to the left of the center of the frame.
+        if detection_midpoint < midpoint_frame - self.tolerance: 
+            yaw = -1
+        # If detection is to the right of the center of the frame.
+        elif detection_midpoint > midpoint_frame + self.tolerance:
+            yaw = 1
+        else:
+            yaw = 1
+
+        return yaw
+    
+    def strafe_smart(self, detection_x):
+        """Strafe to align with the correct side of the gate based on target x_coordinate."""
 
     def run(self, frame, target, detections):
         """
@@ -57,8 +83,28 @@ class CV:
         forward = 0
         lateral = 0
         yaw = 0
-        end = 0
 
-        for detection in detections:
-            print(f"Detection label: {detection.label}")
-        return {"lateral": lateral, "forward": forward, "yaw": yaw, "end": end}, frame
+        # If there are zero detections, yaw
+        # If there is one detection, note on which side of the screen it is and then yaw accordingly (offsource to a different function)
+        # If there are two detections, check confidences and label, then begin strafe.
+        # Once aligned, end.
+
+        if len(detections) == 0:
+            yaw = 1
+        elif len(detections) == 1:
+            detection = detections[0]
+            yaw = self.yaw_smart(detection)
+        elif len(detections) == 2:
+            for detection in detections:
+                x_midpoint = (detection.xmin + detection.xmax)/2 
+                if detection.confidence > 0.5 and target in detection.label:
+                    target_x = x_midpoint
+                elif detection.confidence > 0.5 and target not in detection.label:
+                    other_x = x_midpoint
+                else:
+                    print(f"[WARN] Detections have low confidence, going for the highest confidence label.")
+        
+
+
+            
+        return {"lateral": lateral, "forward": forward, "yaw": yaw, "end": self.end}, frame
