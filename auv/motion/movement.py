f87fd4291c9627a5b91123d5d6b425c31bb82f46
CS50HarvardKC <networkmail.kc@gmail.com>
2024-07-08 15:07:52 -0700

    Added test script for movement() in robot_control

---
+++ auv/motion/movement.py
@@ -0,0 +1,16 @@
+import time
+import rospy
+from . import robot_control
+from  ..utils import arm, disarm
+
+rospy.init_node("Depth")
+
+rc = robot_control.RobotControl()
+
+arm.arm()
+start_time = time.time()
+
+while time.time() - start_time < 10:
+	rc.movement(lateral=0, forward=1, yaw=0)
+
+disarm.disarm()
