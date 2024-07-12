9c15904a97ded6d5d1038d52a049e59ef6ed32c8
CS50HarvardKC <153961673+CS50HarvardKC@users.noreply.github.com>
2024-07-12 14:37:16 -0700

    Update buoy_cv.py

--- auv/cv/buoy_cv.py
+++ auv/cv/buoy_cv.py
@@ -144,8 +144,7 @@ class CV:
         yaw = 0
         end = self.end
         x_tolerance = 20 # Pixels
-        if raw_frame:
-            print("[INFO] Raw frame exists.")
+
         data_from_detection, frame = self.detect_buoy(raw_frame)
 
         if frame is not None:
