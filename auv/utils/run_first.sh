15d92becf338f34d951ff6d08e7315de9f6635d0
CS50HarvardKC <153961673+CS50HarvardKC@users.noreply.github.com>
2024-07-12 19:54:09 -0700

    Update run_first.sh

--- auv/utils/run_first.sh
+++ auv/utils/run_first.sh
@@ -9,7 +9,23 @@ sleep 3
 # Source ROS setup.bash
 screen -S roscore -X stuff 'source /opt/ros/noetic/setup.bash\n'
 
-# Start mavros in another screen session
+# Start MAVROS in another screen session
 screen -dmS mavros bash -c 'source /opt/ros/noetic/setup.bash; roslaunch mavros px4.launch'
 
-echo "ROS nodes are now running in separate screens."
+# Wait for MAVROS to start
+sleep 3
+
+# Update the RoboSub repository to the latest version
+cd /home/inspiration/auv
+git pull
+
+# Run camsVersatile
+screen -dmS cams bash -c 'cd /home/inspiration/auv; python3 -m auv.device.camsVersatile'
+
+# Run pix_standalone
+screen -dmS pix bash -c 'cd /home/inspiration/auv; python3 -m auv.device.pix_standalone'
+
+# Wait for pix_standalone and camsVersatile to be up and running
+sleep 3
+
+echo "ROS nodes and dependencies for autonomous missions are now running in separate screens."
