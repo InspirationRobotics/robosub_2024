b9fb1ab0be5d24a2b29b02c86303dcd1e9e64d48
CS50HarvardKC <153961673+CS50HarvardKC@users.noreply.github.com>
2024-07-12 20:15:25 -0700

    Update movement.py with more exaggerated motion

--- auv/motion/movement.py
+++ auv/motion/movement.py
@@ -10,11 +10,11 @@ rc = robot_control.RobotControl()
 arm.arm()
 start_time = time.time()
 
-rc.set_depth(0.7)
+rc.set_depth(1.2)
 print("[INFO] Beginning test")
 
 while time.time() - start_time < 15:
-	rc.movement(lateral=1, forward=0, yaw=0)
+	rc.movement(lateral=0, forward=1, yaw=0)
 	# Forward is actually a yaw (undetermined which way)
 	# Lateral is
 	# Yaw is 
