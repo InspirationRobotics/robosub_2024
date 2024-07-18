acd6b521e4b85229228ad3b22b920ecce2094337
Leonard Wright <leonardalwright@gmail.com>
2024-07-18 15:40:07 -0700

    Double margin of tolerance

--- auv/cv/buoy_cv.py
+++ auv/cv/buoy_cv.py
@@ -19,7 +19,7 @@ class CV:
         self.shape = (640, 480)
         self.midpoint = self.shape[0] / 2
         self.frame_area = self.shape[0] * self.shape[1]
-        self.tolerance = 50 # Pixels
+        self.tolerance = 100 # Pixels
 
         self.detected = False
         self.prev_detected = False
