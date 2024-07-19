882c3b8b40ca0753cd47226122ea727581394c05
CS50HarvardKC <153961673+CS50HarvardKC@users.noreply.github.com>
2024-07-19 15:42:37 -0700

    Update buoy_cv.py with new tuned values for Onyx (to increase the speed)

--- auv/cv/buoy_cv.py
+++ auv/cv/buoy_cv.py
@@ -125,13 +125,13 @@ class CV:
                 # Once approach is centered, AUV approaches the buoy
                 yaw = 0
                 if buoy_area < 5000:
-                    forward = 1.0
+                    forward = 2.5
                 elif buoy_area < 10000:
-                    forward = 0.75
+                    forward = 1.5
                 elif buoy_area < 15000: # number of pixels in buoy's bounding box
-                    forward = 0.5
+                    forward = 1.0
                 elif buoy_area > 17500:
-                    forward = -0.5
+                    forward = -0.75
             print(f"[INFO] Frame area : {self.frame_area}")
             print(f"[INFO] Buoy area : {buoy_area}")
 
