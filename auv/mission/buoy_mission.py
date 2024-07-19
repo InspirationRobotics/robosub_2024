2fdbf0bc3fc868baaca102f7f6699ca945dd9922
Avika Prasad <avikapd22@gmail.com>
2024-07-19 16:18:53 -0700

    motion changed

--- auv/mission/buoy_mission.py
+++ auv/mission/buoy_mission.py
@@ -119,13 +119,13 @@ class BuoyMission:
             while time.time() - self.first_time < 0.55:
                 self.robot_control.movement(yaw = movement_list[2])
             self.sleep()
-        while time.time() - self.first_time < 1.0:
+        while time.time() - self.first_time < 0.75:
             self.robot_control.movement(lateral = -movement_list[0])
         self.sleep()
-        while time.time() - self.first_time < 1.0:
+        while time.time() - self.first_time < 1.55:
             self.robot_control.movement(forward = -movement_list[1])
         self.sleep()
-        while time.time() - self.first_time < 2:
+        while time.time() - self.first_time < 0.55:
             self.robot_control.movement(yaw = -movement_list[2])
         self.sleep()
         
