cd193e63218a2ea629a81073fb88910d2f20277a
Leonard Wright <leonardalwright@gmail.com>
2024-07-20 14:40:28 -0700

    Tuning

--- auv/cv/buoy_cv.py
+++ auv/cv/buoy_cv.py
@@ -128,11 +128,11 @@ class CV:
                 # Once approach is centered, AUV approaches the buoy
                 yaw = 0
                 if buoy_area < 30000:
-                    forward = 3.0
+                    forward = 2.0
                 elif buoy_area < 40000: # number of pixels in buoy's bounding box
-                    forward = 1.25
+                    forward = 1.0
                 elif buoy_area > 45000:
-                    forward = -1.0
+                    forward = -2.0
             print(f"[INFO] Frame area : {self.frame_area}")
             print(f"[INFO] Buoy area : {buoy_area}")
 
