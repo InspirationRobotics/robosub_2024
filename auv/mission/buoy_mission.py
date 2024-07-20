5670b5528373ecf9f3824b8a8337d55e3d3018f4
Leonard Wright <leonardalwright@gmail.com>
2024-07-20 14:14:03 -0700

    tune circumnavigate

--- auv/mission/buoy_mission.py
+++ auv/mission/buoy_mission.py
@@ -110,11 +110,11 @@ class BuoyMission:
             movement_list = [4, 2.5, -2] # lateral, forward, yaw
         # First move laterally, then move around the buoy
         time.sleep(2.0)
-        while time.time() - self.first_time < 4:
+        while time.time() - self.first_time < 3.5:
             self.robot_control.movement(lateral = movement_list[0])
         self.sleep()
         for i in range(4):
-            while time.time() - self.first_time < 4.0:
+            while time.time() - self.first_time < 3.5:
                 self.robot_control.movement(forward = movement_list[1])
             self.sleep()
             # More than 1.0 to counteract slight left skew of forward movement
@@ -124,7 +124,7 @@ class BuoyMission:
         while time.time() - self.first_time < 0.75:
             self.robot_control.movement(lateral = -movement_list[0]/2)
         self.sleep()
-        while time.time() - self.first_time < 4:
+        while time.time() - self.first_time < 3.5:
             self.robot_control.movement(yaw = -movement_list[2])
         self.sleep()
 
