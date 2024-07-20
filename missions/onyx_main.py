31ec2a3acc068fbb86185530cf58d40d2afbf14d
Leonard514 <leonardalwright@gmail.com>
2024-07-20 15:20:40 -0700

    Revert "Tuning"
    This reverts commit dcb287de99e79e0beca3132a3f964c85b71da36a.

--- missions/onyx_main.py
+++ missions/onyx_main.py
@@ -42,7 +42,7 @@ marker_mission.cleanup()
 
 first_time = time.time()
 
-while time.time() - first_time < 20:
+while time.time() - first_time < 18:
     rc.movement(forward = movement_list[1])
 
 time.sleep(1)
