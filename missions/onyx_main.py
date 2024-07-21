73212e29eab7f1ee6d07f180b4f5a7df853f8acb
CS50HarvardKC <153961673+CS50HarvardKC@users.noreply.github.com>
2024-07-21 16:52:12 -0700

    Tuning first forward value

--- missions/onyx_main.py
+++ missions/onyx_main.py
@@ -28,7 +28,7 @@ first_time = time.time()
 # move forward for 8 secs
 
 
-while time.time() - first_time < 22:
+while time.time() - first_time < 20:
     rc.movement(forward = movement_list[1])
 
 time.sleep(2)
