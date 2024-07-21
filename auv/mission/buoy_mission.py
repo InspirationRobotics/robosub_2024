3f3657d316e75f40c2f72e6f461d885f6f709d95
CS50HarvardKC <networkmail.kc@gmail.com>
2024-07-20 21:08:59 -0700

    Tuning yaw values

--- auv/mission/buoy_mission.py
+++ auv/mission/buoy_mission.py
@@ -115,11 +115,11 @@ class BuoyMission:
             self.robot_control.movement(yaw = -movement_list[2])
         self.sleep()
         # 1st forward
-        while time.time() - self.first_time < 2.5:
+        while time.time() - self.first_time < 2:
             self.robot_control.movement(forward = movement_list[1])
         # Yaw for the gate
         self.sleep()
-        while time.time() - self.first_time < 1.4:
+        while time.time() - self.first_time < 2.0:
             self.robot_control.movement(yaw = -movement_list[2])
         self.sleep()
         # # 2nd forward
