91642d478280c6c4c14d41252cd0818b2771735f
CS50HarvardKC <153961673+CS50HarvardKC@users.noreply.github.com>
2024-07-21 17:16:41 -0700

    Tuning first and second forward values

--- missions/onyx_main.py
+++ missions/onyx_main.py
@@ -28,7 +28,7 @@ first_time = time.time()
 # move forward for 8 secs
 
 
-while time.time() - first_time < 20:
+while time.time() - first_time < 21:
     rc.movement(forward = movement_list[1])
 
 time.sleep(2)
@@ -42,7 +42,7 @@ marker_mission.circumnavigate()
 
 first_time = time.time()
 
-while time.time() - first_time < 25:
+while time.time() - first_time < 10:
     rc.movement(forward = movement_list[1])
 
 time.sleep(1)
