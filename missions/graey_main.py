ca2b886c8b5576fce3541c024a36bc4d7b96b184
CS50HarvardKC <networkmail.kc@gmail.com>
2024-07-21 20:25:55 -0700

    Beginning prequalification forward tuning

--- missions/graey_main.py
+++ missions/graey_main.py
@@ -16,7 +16,7 @@ rospy.init_node("prequal_mission", anonymous = True)
 marker_mission = buoy_mission.BuoyMission()
 rc = robot_control.RobotControl()
 
-movement_list = [-2, 2, 1] # lateral, forward, yaw
+movement_list = [-2, 1.5, 1] # lateral, forward, yaw
 first_time = time.time()
 
 
@@ -24,7 +24,7 @@ arm.arm()
 
 # move forward for 8 secs
 
-while time.time() - first_time < 8:
+while time.time() - first_time < 10:
     rc.movement(forward = movement_list[1])
 
 time.sleep(1)
@@ -32,15 +32,15 @@ time.sleep(1)
 
 # Run buoy mission
 
-marker_mission.run()
-marker_mission.cleanup()
+# marker_mission.run()
+# marker_mission.cleanup()
 
 # Move forward for 8 secs
 
 first_time = time.time()
 
-while time.time() - first_time < 8:
-    rc.movement(forward = movement_list[1])
+while time.time() - first_time < 10:
+    rc.movement(forward = - movement_list[1])
 
 time.sleep(1)
 
