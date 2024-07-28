ca4f35fc45b24ef8bb7dac7a9a3d3c9863ad5774
Leonard Wright <leonardalwright@gmail.com>
2024-07-27 20:04:03 -0700

    Make a motion test

---
+++ auv/motion/motion_test.py
@@ -0,0 +1,25 @@
+import rospy
+import time
+from auv.motion import robot_control
+from auv.utils import arm, disarm
+
+
+rospy.init_node("MotionTest", anonymous=True)
+rc = robot_control.RobotControl(enable_dvl=False)
+
+
+first_time = time.time()
+while time.time() - first_time < 3:
+    rc.movement(forward = 2)
+
+first_time = time.time()
+while time.time() - first_time < 3:
+    rc.movement(forward = -2)
+
+first_time = time.time()
+while time.time() - first_time < 3:
+    rc.movement(lateral = 2)
+
+first_time = time.time()
+while time.time() - first_time < 3:
+    rc.movement(lateral = -2)
