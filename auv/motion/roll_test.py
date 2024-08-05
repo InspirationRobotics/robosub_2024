9fbd184d4e526616e4acb293bbabb2f8f4625791
CS50HarvardKC <networkmail.kc@gmail.com>
2024-08-04 17:24:01 -0700

    Added roll test

---
+++ auv/motion/roll_test.py
@@ -0,0 +1,31 @@
+import rospy
+import time
+from auv.motion import robot_control
+from auv.utils import arm, disarm
+
+
+rospy.init_node("Roll Test", anonymous=True)
+rc = robot_control.RobotControl(enable_dvl=False)
+
+arm.arm()
+time.sleep(3.0)
+
+first_time = time.time()
+while time.time() - first_time < 10:
+    rc.movement(roll = 1.0)
+
+# first_time = time.time()
+# while time.time() - first_time < 3:
+#     rc.movement(forward = -2)
+
+# first_time = time.time()
+# while time.time() - first_time < 3:
+#     rc.movement(lateral = 2)
+
+# first_time = time.time()
+# while time.time() - first_time < 3:
+#     rc.movement(lateral = -2)
+
+time.sleep(1.0)
+
+disarm.disarm()
