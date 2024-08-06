34034671dce2ea5e68ed29af9dc6e2b68c9e227c
CS50HarvardKC <networkmail.kc@gmail.com>
2024-08-06 14:29:10 -0700

    Buoy mission change

--- auv/cv/data/video_to_frames.py
+++ auv/cv/data/video_to_frames.py
@@ -1,9 +1,9 @@
 import cv2
 import os
 
-video_root_path = "/home/kc/Desktop/Team Inspiration/RoboSub 2024/Training Data/"
-mission_name = "Gate/"
-video_name = "Gate Video 1.mp4"
+video_root_path = r"C:\Users\netwo\Downloads"
+mission_name = r"Octagon_Competition"
+video_name = r"Octagon Comp Video 0.mp4"
 video_path = os.path.join(video_root_path, mission_name, video_name)
 
 video = cv2.VideoCapture(video_path)
@@ -26,8 +26,8 @@ while True:
         break
 
     frame_count += 1
-    if frame_count > 2:
-        break
+    # if frame_count > 2:
+    #     break
     frame_path = os.path.join(folder_path, f'frame_{frame_count:04d}.jpg')
     cv2.imwrite(frame_path, frame)
     print(frame_count)
