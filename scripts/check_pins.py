75d466c3dfc2617bf9318fa0bed8646c64885175
Leonard Wright <leonardalwright@gmail.com>
2024-08-08 16:25:14 -0700

    Change pins

--- scripts/check_pins.py
+++ scripts/check_pins.py
@@ -2,33 +2,27 @@ import RPi.GPIO as GPIO
 import time
 
 # Pin Definitions (BCM numbering)
-# input_pins = [4, 17, 27, 22]  # Replace with your specific pins
+input_pins = [3, 5, 7, 8,
+              10, 11, 12, 13, 15, 16, 18, 19,
+              21, 22, 22, 23, 24, 26, 27, 28, 29,
+              31, 32, 33, 35, 36, 37, 38,
+              40]  # Replace with your specific pins
 
 # Set up the GPIO channel
 GPIO.setmode(GPIO.BOARD)
-# for pin in range(40):
-#     GPIO.setup(pin + 1, GPIO.IN)
+for pin in input_pins:
+    GPIO.setup(pin, GPIO.IN)
 
-# try:
-#     while True:
-#         for pin in range(40):
-#             if GPIO.input(pin + 1):
-#                 print(f"Pin {pin} is HIGH")
-#             else:
-#                 print(f"Pin {pin} is LOW")
-#         time.sleep(1)
+try:
+    while True:
+        for pin in input_pins:
+            if GPIO.input(pin):
+                print(f"Pin {pin} is HIGH")
+            else:
+                print(f"Pin {pin} is LOW")
+        time.sleep(1)
 
-# except KeyboardInterrupt:
-#     print("Program terminated")
-# finally:
-#     GPIO.cleanup()
-
-
-
-
-GPIO.setup(19, GPIO.IN)
-
-if GPIO.input(19):
-    print(f"Pin is high, input {GPIO.input(19)}")
-else:
-    print(f"Pin is low, input {GPIO.input(19)}")
+except KeyboardInterrupt:
+    print("Program terminated")
+finally:
+    GPIO.cleanup()
