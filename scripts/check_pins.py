5e2cf9f94c76b9d43632149faac44bd1dd6022d4
Leonard Wright <leonardalwright@gmail.com>
2024-08-08 16:28:17 -0700

    Remove 27

--- scripts/check_pins.py
+++ scripts/check_pins.py
@@ -4,7 +4,7 @@ import time
 # Pin Definitions (BCM numbering)
 input_pins = [7,
               11, 12, 13, 15, 16, 18, 19,
-              21, 22, 22, 23, 24, 26, 27, 28, 29,
+              21, 22, 22, 23, 24, 26, 28, 29,
               31, 32, 33, 35, 36, 37, 38,
               40]  # Replace with your specific pins
 
