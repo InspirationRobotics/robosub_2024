aa1f525257e711ad94bc5850fb5d6804fb37348d
Leonard Wright <leonardalwright@gmail.com>
2024-08-08 16:20:08 -0700

    Check 37

--- scripts/check_pins.py
+++ scripts/check_pins.py
@@ -26,9 +26,9 @@ GPIO.setmode(GPIO.BOARD)
 
 
 
-GPIO.setup(36, GPIO.IN)
+GPIO.setup(37, GPIO.IN)
 
-if GPIO.input(36):
-    print(f"Pin is high, input {GPIO.input(36)}")
+if GPIO.input(37):
+    print(f"Pin is high, input {GPIO.input(37)}")
 else:
-    print(f"Pin is low, input {GPIO.input(36)}")
+    print(f"Pin is low, input {GPIO.input(37)}")
