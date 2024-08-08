e4c403ee084a839ed91503e1b50032263ecf310f
Leonard Wright <leonardalwright@gmail.com>
2024-08-08 16:34:19 -0700

    Make a list

--- scripts/check_pins.py
+++ scripts/check_pins.py
@@ -2,28 +2,28 @@ import RPi.GPIO as GPIO
 import time
 
 # Pin Definitions (BCM numbering)
-# input_pins = [7,
-#               11, 12, 13, 15, 16, 18, 19,
-#               21, 22, 22, 23, 24, 26, 28, 29,
-#               31, 32, 33, 35, 36, 37, 38,
-#               40]  # Replace with your specific pins
+input_pins = [1, 2, 3, 4, 5, 6, 7, 8, 9,
+              10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+              20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+              30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+              40]  # Replace with your specific pins
 
 # Set up the GPIO channel
 GPIO.setmode(GPIO.BOARD)
 
 try:
-    for pin in range(40):
-        GPIO.setup(pin + 1, GPIO.IN)
+    for pin in input_pins:
+        GPIO.setup(pin, GPIO.IN)
 except Exception:
     pass
 
 try:
     while True:
-        for pin in range(40):
-            if GPIO.input(pin + 1):
-                print(f"Pin {pin + 1} is HIGH")
+        for pin in input_pins:
+            if GPIO.input(pin):
+                print(f"Pin {pin} is HIGH")
             else:
-                print(f"Pin {pin + 1} is LOW")
+                print(f"Pin {pin} is LOW")
         time.sleep(1)
 except Exception:
     pass
