52c04f93edbe58e26dcffb73a6ba13bed9878aa4
Leonard Wright <leonardalwright@gmail.com>
2024-08-09 10:49:13 -0700

    Type correction

--- auv/device/pix2_standalone.py
+++ auv/device/pix2_standalone.py
@@ -196,7 +196,9 @@ class AUV(RosHandler):
         Returns:
             result.mode_sent (str): The mode that was sent to autopilot to set the new mode
         """
-        mode = str(mode)
+        if not isinstance(mode, str):
+            mode = str(mode)
+            mode = mode[1:-1]
         print(f"[DEBUG] Set mode to {mode}")
         # Handle althold specially, setting mode to hold depth and to stabalize to be the new modes
         if mode == MODE_ALTHOLD:
