580f4c4740881aa26c72a8a8e332213e36f8791e
CS50HarvardKC <networkmail.kc@gmail.com>
2024-08-10 14:43:09 -0700

    Adding cointoss

--- missions/gate.py
+++ missions/gate.py
@@ -15,6 +15,9 @@ arm.arm()
 rc.set_depth(0.65)
 time.sleep(5.0)
 
+gate_heading = 220
+rc.set_heading(gate_heading)
+
 gateMission = gate_mission.GateMission(**config)
 gateMission.run()
 gateMission.cleanup()
