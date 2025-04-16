c3074c13341a72a62155371158850acb38140183
Chase-520 <chace070528@gmail.com>
2025-04-16 16:28:34 -0700

    back to old version used threading

--- auv/device/fog/fog_interface.py
+++ auv/device/fog/fog_interface.py
@@ -1,166 +1,253 @@
-#!/usr/bin/env python3
-
 import time
+import threading
 import rospy
-import serial
 from std_msgs.msg import Float64
+
+# use module pyserial (not serial)
+
+import serial
 from auv.utils import deviceHelper
+
+
+# Requires FOG USB to be connected to 
+# lower-right USB port on Jetson to work
 
 fog_port = deviceHelper.dataFromConfig("fog")
 
-class FOGNode:
-    def __init__(self, port='/dev/ttyUSB0'):
-        rospy.init_node("fog_node", anonymous=True)
-        self.ser = self._setupSerial(port)
+class FOG:
 
+    """Allows for tracking of angular movement"""
+
+    def __init__(self, port='/dev/ttyUSB0'):
+        """Initialize the serial connection"""
+        rospy.init_node("FOG", anonymous=True)
+        
+        # Make serial stuff???
+        self.ser        = self._setupSerial(port)
+        self.readData   = False
+
+        self.xDataNames     = ["temp", "supply_voltage", "sld_curr", "diag_sig", "angle_deg"]
+        self.data           = {}
+        self.parsed_data    = {}
+
+        self.samples    = 200
+        self.count      = 0
+        self.angle_sum  = 0
+
+        self.cal_time   = 10 # seconds
+        self.cal_sum    = 0
+        self.cal_count  = 0
+
+        # Previous was 0.0727
+        self.integration_factor = 0.0767 # Determined empirically to be 0.0767 deg/sec/mV (should be close to 1/SF)
+        self.integrated_sum = 0
+        self.bias = 0
+        self.pub_fog = rospy.Publisher("auv/devices/fog", Float64, queue_size=10)
+
+    def _setupSerial(self, p : str) -> serial.Serial:
+        """Make a serial connection to store in self.ser,
+        look more stuff up"""
+        # TODO: Look up serial module, tutorials, etc.
+        # Example of Serial use: https://www.youtube.com/watch?v=WV4U51TlRaQ&list=PLb1SYTph-GZJb1CFM7ioVY9XJYlPVUBQy&index=2&pp=iAQB
+
+        return serial.Serial(
+            port=p, 
+            baudrate=921600, 
+            parity=serial.PARITY_NONE, 
+            stopbits=serial.STOPBITS_ONE, 
+            bytesize=serial.EIGHTBITS)
+    
+    def start_read(self, func = None):
+        """Wipes parameters, sets boolean self.readData to True,
+        initializes a thread of self._read_fog and starts running it"""
+        # Threads exist to allow multiple processes to run simultaneously,
+        # using downtime in one process to run the others. More detailed
+        # info available at https://www.youtube.com/watch?v=A_Z1lgZLSNc
+
+        self.reset_params()
+        self.readData = True
+        self.read_thread =  threading.Thread(target=self._read_fog, args=(func,))
+        self.read_thread.start()
+
+    def reset_params(self):
+        """Change counter variables back to zero, wipe
+        data, and record the current time"""
         self.data = {}
         self.parsed_data = {}
 
-        self.xDataNames = ["temp", "supply_voltage", "sld_curr", "diag_sig", "angle_deg"]
-
-        self.samples = 200
         self.count = 0
         self.angle_sum = 0
 
-        self.cal_time = 10
-        self.cal_sum = 0
-        self.cal_count = 0
-
-        self.integration_factor = 0.0767
         self.integrated_sum = 0
-        self.bias = 0
         self.prev_time = time.time()
 
-        # ROS publishers
-        self.pub_angle = rospy.Publisher("/fog/angle_deg", Float64, queue_size=10)
-        self.pub_temp = rospy.Publisher("/fog/temp", Float64, queue_size=10)
-        self.pub_voltage = rospy.Publisher("/fog/supply_voltage", Float64, queue_size=10)
-        self.pub_sld_curr = rospy.Publisher("/fog/sld_curr", Float64, queue_size=10)
-        self.pub_diag_sig = rospy.Publisher("/fog/diag_sig", Float64, queue_size=10)
-
-        # Calibration first
-        # self.calibrate()
-
-        # Start Timer-based read loop
-        self.line_buffer = []
-        self.prev_line = []
-        rospy.Timer(rospy.Duration(0.001), self.read_callback)
-
-    def _setupSerial(self, p):
-        return serial.Serial(
-            port=p,
-            baudrate=921600,
-            parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE,
-            bytesize=serial.EIGHTBITS
-        )
-
-    def calibrate(self):
-        rospy.loginfo("Calibrating FOG... DO NOT TOUCH")
         self.cal_sum = 0
         self.cal_count = 0
-        start_time = time.time()
-        self.line_buffer = []
-        self.prev_line = []
+    
+    def reset_bias(self):
+        """Change self.bias to zero"""
+        self.bias = 0
 
-        while time.time() - start_time < self.cal_time and not rospy.is_shutdown():
-            self._read_serial_line(self._cal_fog_angle_data)
-
-        self.bias = self.cal_sum / (self.cal_count + 1e-5)
-        rospy.loginfo(f"Calibration complete. Bias: {self.bias:.4f}")
+    def calibrate(self):
+        print("Calibrating FOG... DO NOT TOUCH")
+        self.reset_params()
+        self.start_read(self._cal_fog_angle_data)
+        time.sleep(self.cal_time)
+        self.bias = self.cal_sum/(self.cal_count + 0.00001)
+        self.stop_read()
+        print(f"Calibration complete. Bias: {self.bias}")
 
     def _handle_checksum(self, data):
         if len(data) != 8:
             return False
-        try:
-            checksum = sum(int(data[i], 16) for i in range(1, 6))
-            high_cs = int(data[6], 16)
-            low_cs = int(data[7], 16)
-            return ((high_cs << 8) | low_cs) == checksum
-        except:
-            return False
-
+        sum = 0
+        for i in range(1, len(data) - 2):
+            try:
+                sum += int(data[i], 16)
+            except:
+                return False
+        high_cs = int(data[6], 16)  # second to last is high byte
+        low_cs = int(data[7], 16)
+        combined_value = (high_cs << 8) | low_cs
+        return combined_value == sum
+    
     def _twos_complement(self, value):
+        MODULO = 1 << 24
+        #MAX_VALUE = (1 << 23) - 1
+        #value = int(hexstr, 16)
         if value & (1 << 23):
-            value -= (1 << 24)
+            value -= MODULO
         return value
 
-    def _cal_fog_angle_data(self, curr_line, _):
+    def _cal_fog_angle_data(self, curr_line, PLACEHOLDER):
+        """Counterclockwise angles are negative, clockwise 
+        angles are positive. Angles in degrees (not radians)"""
+        # Calibrate the angle data
         if not self._handle_checksum(curr_line):
             return
         angle_data = int(curr_line[2], 16) << 16 | int(curr_line[3], 16) << 8 | int(curr_line[1], 16)
+            # Converts to 24 bit signed integer and adds to the sum
         self.cal_sum += self._twos_complement(angle_data)
         self.cal_count += 1
 
     def _parse_fog_data(self, curr_line, prev_line):
+        # 0 is the sync bit, 1-3 is the angle data (low high middle), 
+        # 4 is the packet_count, 5 is the xData (even count is high, 
+        # odd count is low), 6 checksum high, 7 checksum low
+
+        # Checksum verification
         if not self._handle_checksum(curr_line) or not self._handle_checksum(prev_line):
             return
-
+        
+        # Packet count
         packet_count = int(curr_line[4], 16)
 
+        # Angle data
         angle_data = int(curr_line[2], 16) << 16 | int(curr_line[3], 16) << 8 | int(curr_line[1], 16)
-        self.angle_sum += (self._twos_complement(angle_data) - self.bias)
+            # Converts to 24 bit signed integer and adds to the sum
+        self.angle_sum += (self._twos_complement(angle_data)-self.bias)
+        #print(f"Angle: {self.twos_complement(angle_data)} Raw: {curr_line[1]} {curr_line[2]} {curr_line[3]}")
+            # Average the angle data
         self.count += 1
-
         if self.count >= self.samples:
-            angle_mv = (self.angle_sum / self.samples) * (2.5 / (2**23))
-            angle_deg_sec = angle_mv * self.integration_factor
-            now = time.time()
-            dt = now - self.prev_time
-            self.integrated_sum += angle_deg_sec * dt
+            angle_mv = (self.angle_sum/self.samples)*(2.5/(2**23)) # Converts to mV
+            angle_deg_sec = angle_mv*self.integration_factor
+            self.integrated_sum += angle_deg_sec/(time.time() - self.prev_time)
             self.parsed_data["angle_deg"] = self.integrated_sum
-            self.prev_time = now
+            self.publish_reading(self.integrated_sum)
+            time.sleep(0.1)
+            self.prev_time = time.time()
             self.angle_sum = 0
             self.count = 0
 
+        # XData
         if packet_count % 2 != 0 and packet_count < 8:
             xData = int(prev_line[5], 16) << 8 | int(curr_line[5], 16)
-            self.data[self.xDataNames[packet_count // 2]] = xData
+            self.data[self.xDataNames[packet_count//2]] = xData
 
+        # Translate the data to the correct units
         self._translate_data()
-        self.publish_reading()
 
     def _translate_data(self):
-        if "temp" in self.data:
-            self.parsed_data["temp"] = self.data["temp"] * (250 / (2**15)) - 50
-        if "supply_voltage" in self.data:
-            self.parsed_data["supply_voltage"] = self.data["supply_voltage"] * (10 / (2**15))
-        if "sld_curr" in self.data:
-            self.parsed_data["sld_curr"] = self.data["sld_curr"] * (0.25 / (2**15))
-        if "diag_sig" in self.data:
-            self.parsed_data["diag_sig"] = self.data["diag_sig"] * (2.5 / (2**15))
+        if "temp" in self.data.keys():
+            self.parsed_data["temp"] = self.data["temp"]*(250/(2**15)) - 50 
+        if "supply_voltage" in self.data.keys():
+            self.parsed_data["supply_voltage"] = self.data["supply_voltage"]*(10/(2**15))
+        if "sld_curr" in self.data.keys():
+            self.parsed_data["sld_curr"] = self.data["sld_curr"]*(0.25/(2**15))
+        if "diag_sig" in self.data.keys():
+            self.parsed_data["diag_sig"] = self.data["diag_sig"]*(2.5/(2**15))
 
-    def _read_serial_line(self, callback):
-        while self.ser.in_waiting:
-            byte = self.ser.read(1)
-            if byte == b'\xdd':
-                if len(self.line_buffer) == 8 and (not self.prev_line or self.prev_line[4] != self.line_buffer[4]):
-                    callback(self.line_buffer, self.prev_line)
-                    self.prev_line = self.line_buffer
-                self.line_buffer = [byte.hex()]
-            else:
-                self.line_buffer.append(byte.hex())
+    def _read_fog(self, func = None):
+        """"""
+        # see sample.txt for example data
 
-    def read_callback(self, event):
-        self._read_serial_line(self._parse_fog_data)
+        # make lines, which will be lists of 8 string elements. The first
+        # element is always b'\xdd', the others are two hex digits
+        line = []
+        prev_line = []
 
-    def publish_reading(self):
-        def pub(pub, key):
-            if key in self.parsed_data:
-                pub.publish(Float64(data=self.parsed_data[key]))
+        # Execute while self.readData and self.ser.in_waiting are True
+        while self.readData:
+            while self.ser.in_waiting:
+                if not self.readData: 
+                    break
 
-        pub(self.pub_angle, "angle_deg")
-        pub(self.pub_temp, "temp")
-        pub(self.pub_voltage, "supply_voltage")
-        pub(self.pub_sld_curr, "sld_curr")
-        pub(self.pub_diag_sig, "diag_sig")
+                # Read one byte from the serial connection
+                byte = self.ser.read(1)
 
-    def shutdown(self):
+                # For a given line, append bytes as string elements to a given line (else)
+                # unless b'\xdd' is encountered (proceed to if), whereby if the line
+                # list has 8 elements, and either the first line is being recorded
+                # or the fifth byte of the current line isn't the same as the previous line,
+                # run a function to parse the fog data (or self._parse_fog_data if none are provided).
+                # Save the current line to the previous line and start a new line of bytes with
+                # the b'\xdd' byte
+
+                if byte == b'\xdd':
+                    if len(line) == 8 and (prev_line == [] or prev_line[4] != line[4]):
+                        if func is None: 
+                            self._parse_fog_data(line, prev_line)
+                        else: 
+                            func(line, prev_line)
+                        prev_line = line
+                    line = [byte.hex()]
+                else:
+                    line.append(byte.hex())
+    
+    def publish_reading(self, reading):
+        fog_reading = Float64()
+        fog_reading.data = reading
+        self.pub_fog.publish(fog_reading)
+
+    def stop_read(self):
+        self.readData = False
+        self.read_thread.join()
+        print("FOG read thread stopped.")
+
+    def close(self):
+        if self.readData: self.stop_read()
         self.ser.close()
+        print("FOG serial port closed.")
 
 if __name__ == "__main__":
+    fog = FOG(fog_port)
 
-    fog_node = FOGNode(fog_port)
-    rospy.spin()
-    rospy.on_shutdown(fog_node.shutdown)
+    # Step 1: Calibrate
+    fog.calibrate()
 
+    # Step 2: Start reading
+    print("Now just running FOG data for 30 seconds")
+    fog.start_read()
+    try:
+        while True:
+            if "angle_deg" in fog.parsed_data:
+                print(fog.parsed_data["angle_deg"])
+                fog.publish_reading(fog.parsed_data["angle_deg"])
+            else:
+                print("No FOG data yet.")
+            time.sleep(0.25)
+    except KeyboardInterrupt:
+        print("Stopping FOG data collection...")
+        fog.stop_read()
+        fog.close()
