ef9bebbd04ae302be22e1341766c9c701e00f5d8
Chase-520 <chace070528@gmail.com>
2025-04-16 12:20:12 -0700

    safe exit

--- auv/utils/dataLogging.py
+++ auv/utils/dataLogging.py
@@ -1,74 +1,69 @@
-"""
-Creates a ROS Bag file that records the data from the custom ROS subscribers that detail data from the compass, IMU, barometer,
-thrusters, depth, arm, and mode of the sub
-
-This allows for saving data and playing back data synchronously (since a ROS Bag file saves the timestamps of when the data was published)
-"""
-
 #!/usr/bin/env python
 import os
 import signal
-import threading
 import time
 from datetime import datetime
 
 import rosbag
 import rospy
 import std_msgs.msg
-from mavros_msgs.msg import OverrideRCIn, State
-from mavros_msgs.srv import CommandBool, CommandBoolRequest, SetMode, SetModeRequest
+from mavros_msgs.msg import OverrideRCIn
 from sensor_msgs.msg import FluidPressure, Imu
 
 
-class rosBags:
-    def create_rosbag(self):
+class RosbagRecorder:
+    def __init__(self):
         rospy.init_node("rosbag_creator", anonymous=True)
-        fileName = str(datetime.now())
-        fileName = fileName.split(".")
-        fileName = fileName[0].split(" ")
-        temp = fileName[1].split(":")
-        fileName[1] = f"{temp[0]}-{temp[1]}-{temp[2]}"
-        fileName = f"{fileName[0]}_{fileName[1]}"
-        self.name = f"/home/inspiration/bags/{fileName}.bag"
-        os.system("mkdir /home/inspiration/bags >/dev/null 2>&1")
-        os.system(f"touch {self.name}")
+        self.name = self.generate_filename()
+        os.makedirs("/home/inspiration/bags", exist_ok=True)
         self.bag = rosbag.Bag(self.name, "w")
-        print(f"Successfully made bag file: {self.name}")
+        print(f"✅ Successfully created bag file: {self.name}")
 
-        # Subscribe to the desired topics and save messages to the bag.
-        rospy.Subscriber("/auv/devices/compass", std_msgs.msg.Float64, self.bag_write_callback, callback_args=("/auv/devices/compass"))
-        rospy.Subscriber("/auv/devices/imu", Imu, self.bag_write_callback, callback_args=("/auv/devices/imu"))
-        rospy.Subscriber("/auv/devices/baro", std_msgs.msg.Float32MultiArray, self.bag_write_callback, callback_args=("/auv/devices/baro"))
-        rospy.Subscriber("/auv/devices/thrusters", OverrideRCIn, self.bag_write_callback, callback_args=("/auv/devices/thrusters"))
-        rospy.Subscriber("/auv/devices/setDepth", std_msgs.msg.Float64, self.bag_write_callback, callback_args=("/auv/devices/setDepth"))
-        rospy.Subscriber("/auv/status/arm", std_msgs.msg.Bool, self.bag_write_callback, callback_args=("/auv/status/arm"))
-        rospy.Subscriber("/auv/status/mode", std_msgs.msg.String, self.bag_write_callback, callback_args=("/auv/status/mode"))
-        rospy.spin()
+        # Subscribers
+        self.subs = []
+        self.subs.append(rospy.Subscriber("/auv/devices/compass", std_msgs.msg.Float64, self.bag_write_callback, "/auv/devices/compass"))
+        self.subs.append(rospy.Subscriber("/auv/devices/imu", Imu, self.bag_write_callback, "/auv/devices/imu"))
+        self.subs.append(rospy.Subscriber("/auv/devices/baro", std_msgs.msg.Float32MultiArray, self.bag_write_callback, "/auv/devices/baro"))
+        self.subs.append(rospy.Subscriber("/auv/devices/thrusters", OverrideRCIn, self.bag_write_callback, "/auv/devices/thrusters"))
+        self.subs.append(rospy.Subscriber("/auv/devices/setDepth", std_msgs.msg.Float64, self.bag_write_callback, "/auv/devices/setDepth"))
+        self.subs.append(rospy.Subscriber("/auv/status/arm", std_msgs.msg.Bool, self.bag_write_callback, "/auv/status/arm"))
+        self.subs.append(rospy.Subscriber("/auv/status/mode", std_msgs.msg.String, self.bag_write_callback, "/auv/status/mode"))
 
-    def closeBag(self):
-        # Close the bag file
-        self.bag.close()
+    def generate_filename(self):
+        now = datetime.now()
+        time_str = now.strftime("%Y-%m-%d_%H-%M-%S")
+        return f"/home/inspiration/bags/{time_str}.bag"
 
     def bag_write_callback(self, msg, topic):
-        self.bag.write(topic, msg)
+        try:
+            self.bag.write(topic, msg)
+        except Exception as e:
+            rospy.logerr(f"Failed to write to bag: {e}")
+
+    def close(self):
+        print("\n🛑 Shutting down... closing bag file.")
+        self.bag.close()
+        print(f"📁 Bag saved to {self.name}")
 
 
-def onExit(signum, frame):
-    try:
-        print("\nClosing Rosbag...")
-        rospy.signal_shutdown("Rospy Exited")
-        while not rospy.is_shutdown():
-            pass
-        bag.closeBag()
-        time.sleep(1)
-        print(f"\nBag Saved to {bag.name} ...Done")
-        exit(1)
-    except:
-        pass
+# Global recorder instance
+recorder = None
 
+def handle_shutdown(sig, frame):
+    global recorder
+    if recorder:
+        recorder.close()
+    rospy.signal_shutdown("Shutdown requested")
+    exit(0)
 
-signal.signal(signal.SIGINT, onExit)
+signal.signal(signal.SIGINT, handle_shutdown)
 
 if __name__ == "__main__":
-    bag = rosBags()
-    bag.create_rosbag()
+    recorder = RosbagRecorder()
+
+    try:
+        # Main loop keeps script alive and responsive to shutdown
+        while not rospy.is_shutdown():
+            time.sleep(0.1)
+    except rospy.ROSInterruptException:
+        pass
