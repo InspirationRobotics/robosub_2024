0c9a0defe13dbc743b501af8f26e078efc64b3c1
Chase-520 <chace070528@gmail.com>
2025-04-17 15:34:13 -0700

    simplified rotation node

--- tests/motion_tests/rotate_node.py
+++ tests/motion_tests/rotate_node.py
@@ -1,32 +1,15 @@
-"""
-This is a test script for rc override,
-"""
+#!/usr/bin/env python
+
 import rospy
 from geometry_msgs.msg import TwistStamped
 
-class AUV():
-    def __init__(self):
-        rospy.init_node("yaw_node", anonymous=True)
-        self.pub = rospy.Publisher("/mavros/setpoint_velocity/cmd_vel",TwistStamped, queue_size=10)
-        self.rate = rospy.Rate(10)  # 10 Hz
+rospy.init_node("simple_angular_publisher")
+pub = rospy.Publisher("/mavros/setpoint_velocity/cmd_vel", TwistStamped, queue_size=10)
 
-    def run(self):
-        while not rospy.is_shutdown():
-            vel = TwistStamped()
-            vel.header.stamp = rospy.Time.now()
-            vel.header.frame_id = "base_link"
-            vel.twist.linear.x = 0.0  # move forward
-            vel.twist.linear.y = 0.0
-            vel.twist.linear.z = 0.0
-            vel.twist.angular.x = 0.5
-            vel.twist.angular.y = 0.0
-            vel.twist.angular.z = 0.0
-            self.pub.publish(vel)
-            self.rate.sleep()
+rate = rospy.Rate(10)  # 10 Hz
+msg = TwistStamped()
+msg.twist.angular.x = 0.5
 
-if __name__ == "__main__":
-    try:
-        auv = AUV()
-        auv.run()
-    except rospy.ROSInterruptException:
-        pass
+while not rospy.is_shutdown():
+    pub.publish(msg)
+    rate.sleep()
