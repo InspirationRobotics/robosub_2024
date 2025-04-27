e65b9b9fa70fb5626ba2b38e870db8c48a9ad273
Chase-520 <chace070528@gmail.com>
2025-04-27 11:51:32 -0700

    adding a constant offset

--- auv/localization/localize.py
+++ auv/localization/localize.py
@@ -33,35 +33,38 @@ class Localize:
 
     def imuCallback(self, msg):
         current_time = msg.header.stamp.to_sec()
+        acc_x_offset = 0.9
+        acc_y_offset = 0.35
+        acc_z_offset = 9.98
 
         if self.imu_last_time is None:
             self.imu_last_time = current_time
-            self.acc_x = msg.linear_acceleration.x
-            self.acc_y = msg.linear_acceleration.y
-            self.acc_z = msg.linear_acceleration.z
+            self.acc_x = msg.linear_acceleration.x + acc_x_offset
+            self.acc_y = msg.linear_acceleration.y + acc_y_offset
+            self.acc_z = msg.linear_acceleration.z + acc_z_offset
             return
 
         dt = current_time - self.imu_last_time
 
         # X-axis
         prev_vel = self.imu_vel_x
-        self.imu_vel_x += (self.acc_x + msg.linear_acceleration.x) * dt / 2
+        self.imu_vel_x += (self.acc_x + msg.linear_acceleration.x + acc_x_offset) * dt / 2
         self.imu_pos_x += positionCallback(prev_vel, self.imu_vel_x, dt)
 
         # Y-axis
         prev_vel = self.imu_vel_y
-        self.imu_vel_y += (self.acc_y + msg.linear_acceleration.y) * dt / 2
+        self.imu_vel_y += (self.acc_y + msg.linear_acceleration.y + acc_y_offset) * dt / 2
         self.imu_pos_y += positionCallback(prev_vel, self.imu_vel_y, dt)
 
         # Z-axis
         prev_vel = self.imu_vel_z
-        self.imu_vel_z += (self.acc_z + msg.linear_acceleration.z) * dt / 2
+        self.imu_vel_z += (self.acc_z + msg.linear_acceleration.z + acc_z_offset) * dt / 2
         self.imu_pos_z += positionCallback(prev_vel, self.imu_vel_z, dt)
 
         # Update last acceleration and time
-        self.acc_x = msg.linear_acceleration.x
-        self.acc_y = msg.linear_acceleration.y
-        self.acc_z = msg.linear_acceleration.z
+        self.acc_x = msg.linear_acceleration.x + acc_x_offset
+        self.acc_y = msg.linear_acceleration.y + acc_y_offset
+        self.acc_z = msg.linear_acceleration.z + acc_z_offset
         self.imu_last_time = current_time
 
     def run(self):
