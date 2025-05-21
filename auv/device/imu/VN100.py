489d850146dfd2f0d8c95b6fe82d256828816c88
Leonard Wright <leonardalwright@gmail.com>
2025-05-21 16:53:17 -0700

    Test VN-100 IMU API

--- auv/device/imu/VN100.py
+++ auv/device/imu/VN100.py
@@ -2,13 +2,22 @@ import time
 import threading
 from serial import Serial
 import rospy
-from sensor_msg.msg import Imu
+import sensor_msgs.msg
+
+rospy.init_node("vectornav_api_node")
 
 class VN100:
     def __init__(self,port:str = "dev/ttyUSB0"):
         self.__port = port
         self.__bps = 115200
         self.__ser = Serial(port=self.__port,baudrate=self.__bps, timeout=1)
+        self.vectornav_subscriber = rospy.Subscriber("/vectornav/IMU", sensor_msgs.msg.Imu, self.get_orientation)
+        self.rate = rospy.Rate(1)
+        while not rospy.is_shutdown():
+            self.rate.sleep()
+    
+    def get_orientation(self, msg):
+        print(msg.orientation)
     
     
     
