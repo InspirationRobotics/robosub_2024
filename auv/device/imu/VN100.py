20ca19046447173f925f349907a8821f15772f2b
Leonard Wright <leonardalwright@gmail.com>
2025-05-21 17:01:42 -0700

    Add device helper, fix class

--- auv/device/imu/VN100.py
+++ auv/device/imu/VN100.py
@@ -3,11 +3,12 @@ import threading
 from serial import Serial
 import rospy
 import sensor_msgs.msg
+from auv.utils import deviceHelper
 
 rospy.init_node("vectornav_api_node")
 
 class VN100:
-    def __init__(self,port:str = "dev/ttyUSB0"):
+    def __init__(self,port:str = deviceHelper.dataFromConfig("vectornav_port")):
         self.__port = port
         self.__bps = 115200
         self.__ser = Serial(port=self.__port,baudrate=self.__bps, timeout=1)
@@ -20,6 +21,7 @@ class VN100:
         print(msg.orientation)
     
     
-    
+if __name__ == "__main__":
+    vectornav_sensor = VN100()    
 
 
