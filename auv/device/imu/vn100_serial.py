5a2301419efc312dcbfe0cd0e81e767f5a94a854
Leonard Wright <leonardalwright@gmail.com>
2025-05-23 16:01:05 -0700

    Sampling function

--- auv/device/imu/vn100_serial.py
+++ auv/device/imu/vn100_serial.py
@@ -26,7 +26,7 @@ class VN100:
             # I'll split it by commas to make accessing the data a bit easier
             
             data_list = data_line.split(',')
-            self.yaw, self.pitch, self.roll = data_list[1], data_list[2], data_list[3]
+            self.yaw, self.pitch, self.roll = float(data_list[1]), float(data_list[2]), float(data_list[3])
         except IndexError:
             print("Bad data")
 
@@ -35,11 +35,28 @@ class VN100:
     
 if __name__ == "__main__":
     sensor = VN100()
+
+    # Make some variables to measure how often the IMU is changing its measurements
+    imu_counter = 0
+    imu_sample = []
+    imu_angle_records = None
     # Collect data every second
     while True:
         try:
             sensor.get_orientation()
-            print(f"Roll: {sensor.roll}\nPitch:{sensor.pitch}\nYaw:{sensor.yaw}")
+            print(f"Roll: {sensor.roll}\nPitch:{sensor.pitch}\nYaw:{sensor.yaw}\n")
+            # Increment imu_counter
+            imu_counter += 1
+            # Check if any angles changed more than 5 degrees. First case populates
+            # them for the first iteration
+            if imu_angle_records is None:
+                pass
+            elif abs(sensor.roll - imu_angle_records[0]) < 5 or abs(sensor.pitch - imu_angle_records[1]) < 5 or abs(sensor.yaw - imu_angle_records[2]) < 5:
+                imu_sample.append(imu_counter)
+                imu_counter = 0
+                print(imu_sample)
+            # Populate angles
+            imu_angle_records = [sensor.roll, sensor.pitch, sensor.yaw]
             time.sleep(1)
         except AttributeError:
             print("No data yet")
