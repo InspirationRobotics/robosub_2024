24128c3e4cf63c4755209bcef3075d536eb43c7f
Chase <chace070528@gmail.com>
2025-06-04 23:13:22 -0700

    modify VN100_serial to read ypr, accel, gyro and finalize the api

--- auv/device/imu/vn100_serial.py
+++ auv/device/imu/vn100_serial.py
@@ -1,18 +1,33 @@
 import time
 import threading
 from serial import Serial
-from auv.utils import deviceHelper
+#from auv.utils import deviceHelper
 
 class VN100:
-    def __init__(self,port:str = deviceHelper.dataFromConfig("vectornav")):
+    def __init__(self,port:str = ""): # deviceHelper.dataFromConfig("vectornav")
         """Makes a serial connection to the VN100 IMU utilizing deviceHelper and starts reading"""
         self.__port = port
         self.__bps = 115200
         self.__ser = Serial(port=self.__port,baudrate=self.__bps, timeout=1)
-        self.read_thread = threading.Thread(target=self.get_orientation, daemon=True)
+
+        # Initialize sensor values
+        self.yaw = 0.0
+        self.pitch = 0.0
+        self.roll = 0.0
+        self.accX = 0.0
+        self.accY = 0.0
+        self.accZ = 0.0
+        self.gyroX = 0.0
+        self.gyroY = 0.0
+        self.gyroZ = 0.0
+
+        # start thread
+        self.read_thread = threading.Thread(target=self.read, daemon=True)
         self.read_thread.start()
+
+        time.sleep(2) # sleep for 2s and wait for one iteration in thread
     
-    def get_orientation(self):
+    def read(self):
         """Parses roll, pitch, and yaw from the serial line"""
         # The format of a packet is first $YNYMR, then the yaw, pitch, roll;
         # then magnetometer, accelerometer, and gyroscope;
@@ -28,8 +43,14 @@ class VN100:
                 # I'll split it by commas to make accessing the data a bit easier
                 data_list = data_line.split(',')
 
+                # debug
+                # print(data_list, len(data_list))
+        
                 # Populate yaw, pitch, roll
                 self.yaw, self.pitch, self.roll = (float(data_list[1]) + 90) % 360, float(data_list[2]), float(data_list[3])
+                self.accX, self.accY, self.accZ = float(data_list[4]) , float(data_list[5]), float(data_list[6])
+                self.gyroX, self.gyroY, self.gyroZ = float(data_list[7]) , float(data_list[8]), float(data_list[9])
+
             except IndexError:
                 print("Bad data")
             except Exception:
@@ -42,7 +63,7 @@ if __name__ == "__main__":
     import time
     import csv
     from datetime import datetime
-    sensor = VN100()
+    sensor = VN100(port="COM7")
 
     # Create a timestamped filename
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -60,7 +81,9 @@ if __name__ == "__main__":
                 "Pitch": sensor.pitch,
                 "Yaw": sensor.yaw
             })
-            print(f"Roll: {sensor.roll:.2f}\nPitch: {sensor.pitch:.2f}\nYaw: {sensor.yaw:.2f}\n")
+            print(f"YPR: ({sensor.yaw:.2f}, {sensor.pitch:.2f}, {sensor.roll:.2f})")
+            print(f"Acc: ({sensor.accX:.2f}, {sensor.accY:.2f}, {sensor.accZ:.2f})")
+            print(f"Gyro: ({sensor.gyroX:.2f}, {sensor.gyroY:.2f}, {sensor.gyroZ:.2f})")
 
     except KeyboardInterrupt:
         print("Exiting and saving data to CSV...")
