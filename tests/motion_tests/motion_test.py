2c581daabcaffce377f4286b056a82a1191a4227
HTr04 <huytranlam123@gmail.com>
2025-06-17 13:17:17 -0700

    change lateral time from 4 to 2.5 seconds

--- tests/motion_tests/motion_test.py
+++ tests/motion_tests/motion_test.py
@@ -21,7 +21,7 @@ while time.time() - first_time < 20:
    rc.movement(forward = 2)
 
 first_time = time.time()
-while time.time() - first_time < 4:
+while time.time() - first_time < 2.5:
     rc.movement(lateral = -2)
 
 first_time = time.time()
