f51c7a915eee2232ca5ce447a2f1531e48664a84
avikaprasad22 <avikapd22@gmail.com>
2025-07-03 13:57:13 -0700

    updated specs with usb low-light

--- auv/cv/test/distance_calc_test.py
+++ auv/cv/test/distance_calc_test.py
@@ -1,12 +1,12 @@
 
 
-# oakD pro wide for onyx, oakD wide for graey
+# these measurements are for the USD low-light camera
 
-focal_length = 2.75 # mm
+focal_length = 2.97 # 2.75 mm
 real_pole_width = 25.4 # mm
-image_width_px = 4056 # pixels
+image_width_px =  1433 # 4056 pixels
 object_width_px = input("Enter the width of the object in pixels: ")
-camera_width = 97 # mm
+camera_width = 12.8 # 97 mm
 
 def calculate_distance(object_width_px):
     """
@@ -23,4 +23,4 @@ def calculate_distance(object_width_px):
         print(f"Invalid input: {e}")
         return None
 
-print("distance from sub:", calculate_distance(object_width_px), "mm")
+print("distance from sub:", calculate_distance(object_width_px)/304.8, "ft")
