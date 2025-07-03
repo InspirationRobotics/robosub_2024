410673b75be6fc71e83803d8f64897fd8b54a5bf
avikaprasad22 <avikapd22@gmail.com>
2025-07-03 13:31:03 -0700

    takes width as parameters now

--- auv/cv/test/distance_calc_test.py
+++ auv/cv/test/distance_calc_test.py
@@ -1,24 +1,26 @@
-## all mesaurements are in millimeters/pixels
 
-focal_length = 4.81
-real_pole_height = 914.4 # (3 ft)
-image_height = 800 # (pixels)
-object_height_pixels = input("Enter the height of the object in pixels: ")
-camera_height = 54.5
 
-def calculate_distance(object_height_pixels):
+# oakD pro wide for onyx, oakD wide for graey
+
+focal_length = 2.75 # mm
+real_pole_width = 25.4 # mm
+image_width_px = 4056 # pixels
+object_width_px = input("Enter the width of the object in pixels: ")
+camera_width = 97 # mm
+
+def calculate_distance(object_width_px):
     """
-    Calculate the distance to an object based on its height in pixels.
+    Calculate the distance to an object based on its width in pixels.
     """
     try:
-        object_height_on_cam = float(object_height_pixels)
-        if object_height_on_cam <= 0:
+        object_width_on_cam = float(object_width_px)
+        if object_width_on_cam <= 0:
             raise ValueError("Object height must be a positive number.")
         
-        distance = (focal_length * real_pole_height * image_height) / (object_height_on_cam * camera_height)
+        distance = (focal_length * real_pole_width * image_width_px) / (object_width_on_cam * camera_width)
         return distance
     except ValueError as e:
         print(f"Invalid input: {e}")
         return None
-    
-print("distance from sub:", calculate_distance(object_height_pixels), "mm")
+
+print("distance from sub:", calculate_distance(object_width_px), "mm")
