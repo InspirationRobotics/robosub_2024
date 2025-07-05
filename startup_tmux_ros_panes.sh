a52b8fa0665deaa1db86089cb209e70aab6dea38
Leonard514 <leonardalwright@gmail.com>
2025-07-05 12:13:36 -0700

    update tmux setup script

--- startup_tmux_ros_panes.sh
+++ startup_tmux_ros_panes.sh
@@ -13,11 +13,18 @@ tmux split-window -v -t $SESSION_NAME:0.2
 
 tmux send-keys -t $SESSION:0.0 'echo "Hello from Pane 0"' C-m
 tmux send-keys -t $SESSION:0.1 'echo "Hello from Pane 1"' C-m
-tmux send-keys -t $SESSION:0.1 'roscore' C-m
+tmux send-keys -t $SESSION:0.0 'roscore' C-m
+echo 'starting roscore ...'
+sleep 5
+tmux send-keys -t $SESSION:0.2 'source /opt/ros/${ROS_DISTRO}/setup.bash' C-m
 tmux send-keys -t $SESSION:0.2 'roslaunch mavros px4.launch' C-m
-tmux send-keys -t $SESSION:0.3 'python3 -m auv.device.pix_standalone' C-m
+echo 'starting mavros ...'
+sleep 5
+tmux send-keys -t $SESSION:0.1 'python3 -m auv.device.pix_standalone' C-m
+echo 'starting pix_standalone ...'
+sleep 3
 # Optional: Select the first pane (idle) on attach
-tmux select-pane -t $SESSION_NAME:0.0
+tmux select-pane -t $SESSION_NAME:0.3
 
 # Attach to session
 tmux attach-session -t $SESSION_NAME
