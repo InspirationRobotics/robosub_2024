c6d817df2df2a6e22ebec499aeedfcaddbd72711
Chase <chace070528@gmail.com>
2025-07-06 01:52:40 -0700

    move stuff around and create a start code for maestro server

--- auv/motion/MiniMaestro/ServoNode.py
+++
@@ -1,7 +0,0 @@
-"""
-This is the servo node that controls all servos connected to our pololo mini maestro
-Service names:
-    - dropper : /auv/device/dropper
-    - gripper : /auv/device/gripper
-    - torpedo:  /auv/device/torpedo
-"""
