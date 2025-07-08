0d2d18f3844a4dfbefe5608bedea0d2d139190fa
Chase <chace070528@gmail.com>
2025-07-08 15:10:13 -0700

    remove redundent node name

--- auv/device/imu/vn100_serial.py
+++ auv/device/imu/vn100_serial.py
@@ -9,8 +9,6 @@ from transforms3d.euler import euler2quat
 from auv.utils import deviceHelper
 from sensor_msgs.msg import Imu
 
-rospy.init_node("vectornav_serial_api", anonymous=True)
-
 class VN100:
     def __init__(self, port: str = deviceHelper.dataFromConfig("vectornav")):
         """Makes a serial connection to the VN100 IMU utilizing deviceHelper and starts reading"""
