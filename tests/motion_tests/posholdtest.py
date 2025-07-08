cda1439a8f66d37f47ffc25fd81b533eed472ef5
Chase <chace070528@gmail.com>
2025-07-08 15:04:39 -0700

    protect code with __name__ == __main__

--- tests/motion_tests/posholdtest.py
+++ tests/motion_tests/posholdtest.py
@@ -2,16 +2,16 @@ import rospy
 import time
 from auv.motion import robot_control
 from auv.utils import arm, disarm
+if __name__ == "__main__":
+    rospy.init_node("poshold_test", anonymous=True)  # avoid hiearchy issue
+    rc = robot_control.RobotControl(debug=True)
 
-rospy.init_node("poshold_test", anonymous=True)  # avoid hiearchy issue
-rc = robot_control.RobotControl(debug=True)
+    arm.arm()
+    time.sleep(3.0)
+    print("[INFO}This is the start")
+    rc.set_depth(0.9)
 
-arm.arm()
-time.sleep(3.0)
-print("[INFO}This is the start")
-rc.set_depth(0.9)
+    time.sleep(20)
+    print("[INFO] Reached the end of the program")
 
-time.sleep(20)
-print("[INFO] Reached the end of the program")
-
-disarm.disarm()
+    disarm.disarm()
