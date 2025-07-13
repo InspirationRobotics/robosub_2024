e6a417da2d6c1748ac77f1772d8b3a6265aaa473
HTr04 <huytranlam123@gmail.com>
2025-07-13 14:41:32 -0700

    use python2

--- tests/Oak-D/cvdebug.py
+++ tests/Oak-D/cvdebug.py
@@ -1,25 +1,21 @@
+#!/usr/bin/env python2
 import rospy
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 import cv2
 
-
 bridge = CvBridge()
+rospy.init_node("cv_debugger", anonymous=True)
 
-rospy.init_node("MotionTest", anonymous=True)
 def callbacks(msg):
-    # check if a msg is convertable?
     try:
-        # Convert ROS Image to OpenCV image (BGR8 encoding)
         cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
     except CvBridgeError as e:
         rospy.logerr(f"cv_bridge error: {e}")
         return
+    cv2.imshow("OAK-D forward", cv_image)
+    cv2.waitKey(1)
 
 sub = rospy.Subscriber('/auv/camera/videoOAKdRawForward', Image, callbacks)
-
 rospy.spin()
-
-
-
-
+cv2.destroyAllWindows()
