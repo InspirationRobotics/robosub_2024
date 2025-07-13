c13c8f3ed9a23f344463dba6e56518afa0c25628
avikaprasad22 <avikapd22@gmail.com>
2025-07-13 10:33:27 -0700

    tuned the hsv values to match buoy

--- auv/cv/poles_cv.py
+++ auv/cv/poles_cv.py
@@ -45,10 +45,10 @@ class CV:
 
     def detect_red_pole(self, frame):
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        lower_red1 = np.array([0, 100, 100])
+        lower_red1 = np.array([0, 120, 70])
         upper_red1 = np.array([10, 255, 255])
-        lower_red2 = np.array([160, 100, 100])
-        upper_red2 = np.array([179, 255, 255])
+        lower_red2 = np.array([170, 120, 70])
+        upper_red2 = np.array([180, 255, 255])
 
         mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
         mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
