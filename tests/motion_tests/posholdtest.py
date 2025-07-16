f59b6f71e2a75439d840ab01ce1358eecba084af
Chase <chace070528@gmail.com>
2025-07-16 01:01:54 -0700

    update posholdtest

--- tests/motion_tests/posholdtest.py
+++ tests/motion_tests/posholdtest.py
@@ -9,46 +9,19 @@ if __name__ == "__main__":
 
         # rospy.loginfo(f"current state: {rc.orientation}")
 
-        rc.set_control_mode("pid")
+        rc.set_control_mode("depth_hold")
         arm.arm()
         time.sleep(3.0)
         print("[INFO}This is the start")
         # rc.set_absolute_z(0.5)
         rc.set_absolute_z(0.5)
         rospy.loginfo("starting...")
-        time.sleep(5)
-
-        rc.set_absolute_yaw(180)
         time.sleep(10)
 
+        rc.set_absolute_yaw(90)
+        time.sleep(10)
         rc.reset()
-
-        # rospy.loginfo(f"current state: {rc.orientation}")
-        # rc.set_control_mode('direct')
-        # time.sleep(1)
-
-        # rc.movement(yaw=1)
-        # rospy.loginfo("yaw at pwm=1")
-        # time.sleep(10)
-
-        # rc.set_control_mode('pid')
-        # rospy.loginfo("Set mode back to pid")
-        # time.sleep(1)
-
-        rc.set_absolute_z(2)
-        # rospy.loginfo("set absolute depth to 2")
-        # time.sleep(20)
-
-        # rc.set_absolute_yaw(180)
-        # rc.set_absolute_x(1.7)
-        # rc.set_absolute_y(2)
-        # rc.set_absolute_z(0.8)
-        # rospy.loginfo("set absolute x to 2, y to 3, z to 0.8, heading to 153")
-        # time.sleep(10)
-
-        # rospy.loginfo(f"current state: {rc.orientation}")
         rospy.loginfo("Reached the end of the program")
-
         rc.exit()
         disarm.disarm()
     except KeyboardInterrupt:
